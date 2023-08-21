1f9821d029732b2dade84a0833c6da2d980b430b
antoshka301 <85652770+antoshka301@users.noreply.github.com>
2023-08-20 20:54:54 -0400

    Update client.js

--- db/client.js
+++ db/client.js
@@ -4,23 +4,31 @@ const { Client } = require('pg');
 const DB_NAME = 'pottery';
 
 const DB_URL =
-  process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
+  process.env.DATABASE_URL || `postgres://pottery:pottery-db.cc5iuj8cvbh1.us-east-2.rds.amazonaws.com/${DB_NAME}`;
 
 let client;
 
 // Github actions client config
-if (process.env.CI) {
-  client = new Client({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'postgres',
-  });
-} else {
-  // local / heroku client config
-  client = new Client(DB_URL);
-}
+// if (process.env.CI) {
+//   client = new Client({
+//     host: 'localhost',
+//     port: 5432,
+//     user: 'postgres',
+//     password: 'postgres',
+//     database: 'postgres',
+//   });
+// } else {
+//   // local / heroku client config
+//   client = new Client(DB_URL);
+// }
+
+client = new Client({
+  host: 'pottery-db.cc5iuj8cvbh1.us-east-2.rds.amazonaws.com',
+  port: 5432,
+  database: 'postgres',
+  user: 'pottery',
+  password: 'pottery1',
+})
 
 // Export
 module.exports = client;
